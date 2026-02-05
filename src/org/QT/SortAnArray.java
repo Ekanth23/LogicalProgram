0dac7644644554d86ad0033d2222cee4725672c1
Ekanth23 <ekanthn@gmail.com>
2026-02-05 16:15:10 +0530

    request to approve the code update from Ekanth branch

--- src/org/QT/SortAnArray.java
+++ src/org/QT/SortAnArray.java
@@ -18,9 +18,11 @@ public class SortAnArray {
                 }
             }
         }
+        System.out.println("Approve code from Ekanth branch");
         System.out.println("Code from Ekanth_LogicalProgam branch");
         System.out.println("Code from Main Branch");
         System.out.println("sorted Array");
+        
         
         for(int num:arr){
             System.out.print(num+" ");
